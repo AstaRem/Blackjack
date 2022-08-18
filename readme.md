c2c283864b4bde25507ab9289140e54176a8f737
Asta Remeikiene <65591369+AstaRem@users.noreply.github.com>
2022-08-18 18:04:17 +0100

    add functionality

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Blackjack 
+    Card game
+- by pressing Start Game button we start the game by getting 2 cards
+- new card adds another card
+- player is out if they get more than 21 points
+- 21 point is Blackjack
+  
+I have build this little app to learn and practise Javascript loops, objects, if statements and functions
--- script.js
+++ script.js
@@ -1,21 +1,71 @@
 
-let firstCard = 11
-let secondCard = 11
-let sum = firstCard + secondCard 
+let cards = []
+let sum = 0
 let hasBlackjack = false
-let isAlive = true
+let isAlive = false
 let message = ""
-console.log(sum)
-
-if(sum <= 20) {
-    message = "Would you like to draw another card?"
-} else if(sum === 21){
-    message = "You have got Blackjack!!!"
-    hasBlackjack = true
-} else {
-    message = "You are out of game."
-    isAlive = false
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+let player = {
+    name: "Per",
+    chips: 145
 }
-console.log(message)
-console.log(`Your status of staying in game is: ${isAlive}` )
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = `${player.name}: $${player.chips}`
+
+
+function getRandomCard(){
+    let randomNumber = Math.floor(Math.random() * 13) + 1
+    if (randomNumber === 11){
+        return 1
+    } else if (randomNumber > 10 ){
+        return 10
+    } else {
+        return randomNumber
+    }
+}
+
+
+function startGame(){
+    isAlive = true;
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard 
+    renderGame()
+}
+
+function renderGame(){
+    cardsEl.textContent = `Cards: `
+    for (let i = 0; i < cards.length; i++){
+        cardsEl.textContent += ` ${cards[i]}  `
+    }
+    sumEl.textContent = `Sum: ${sum}`
+    if(sum <= 20) {
+        message = "Would you like to draw another card?"
+    } else if(sum === 21){
+        message = "You've got a Blackjack!!!"
+        hasBlackjack = true
+    } else {
+        message = "You are out of game."
+        isAlive = false
+    }   
+    messageEl.textContent = message
+
+    console.log(message)
+    console.log(`Your status of staying in game is: ${isAlive}` )
+
+}
+
+
+function newCard(){
+    if(isAlive === true && hasBlackjack === false){
+        let anotherCard = getRandomCard()
+        sum += anotherCard
+        cards.push(anotherCard)
+        renderGame()
+    
+    }
+}
 
